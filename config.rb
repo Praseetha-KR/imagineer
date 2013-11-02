0ccfce9fd7f38534fc10bc85c59416c4d374a42d
Sharat M R <cosmosgenius@gmail.com>
2013-11-03 00:22:09 +0530

    converted to scss

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/css"
-sass_dir = "assets/sass"
+sass_dir = "assets/scss"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 
