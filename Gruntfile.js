da1611b462d63b4dd6a7bfa1e22a9de1426d757f
Sharat M R <cosmosgenius@gmail.com>
2013-11-04 01:00:03 +0530

    added Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,57 @@
-/*global module:false*/
+/*jslint node: true */
+'use strict';
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    jekyll: {
+      serve : {
+        options : {
+          serve : true,
+          port : 8000,
+          src: 'src',
+          dest : 'dev'
+        }
+      },
+      dev : {
+        options:{
+          src: 'src',
+          dest : 'dev'
+        }
+      },
+      prod : {
+        options:{
+          src: 'src',
+          dest : 'prod'
+        }
+      }
+    },
 
+    concurrent: {
+      target: {
+        tasks: ['jekyll:serve', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+    
     watch: { // for development run 'grunt watch'
       jekyll: {
-      files: ['templates/*.html'],
-      tasks: ['jekyll:dev']
+        files: ['src/about/**/*.html',
+                'src/blog/**/*.html',
+                'src/_layouts/**/*.html',
+                'src/_posts/**/*.markdown'],
+        tasks: ['jekyll:dev']
       }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['watch']);
-
+  grunt.registerTask('default', ['concurrent:target']);
+  grunt.registerTask('production', ['jekyll:prod']);
 };
