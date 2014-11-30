89ffe04a66d5b3fa6a6b4f8a0033a6a9597aea45
Praseetha-KR <voidimagineer@yahoo.com>
2014-11-30 16:36:01 +0530

    Minify css & js

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,14 +36,28 @@ module.exports = function (grunt) {
             }
         },
 
-        concat: {
+        concat: { // concatenate files to single file
             js: {
                 src: ['src/assets/js/vendor/*.js', 'src/assets/js/scripts.js'],
-                dest: 'src/assets/js/main.js'
+                dest: 'src/assets/js/app.js'
             },
             css: {
-                src: ['src/assets/css/vendor/*.css', 'src/assets/css/app.css'],
-                dest: 'src/assets/css/main.css'
+                src: ['src/assets/css/vendor/*.css', 'src/assets/css/main.css'],
+                dest: 'src/assets/css/app.css'
+            }
+        },
+
+        cssmin: {
+            css: {
+                src: 'src/assets/css/app.css',
+                dest: 'src/assets/css/app.min.css'
+            }
+        },
+
+        uglify: {
+            js: {
+                src: 'src/assets/js/app.js',
+                dest: 'src/assets/js/app.min.js'
             }
         },
 
@@ -78,7 +92,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['concat', 'concurrent:target']);
+    grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'concurrent:target']);
     grunt.registerTask('production', ['compass', 'jekyll:prod']);
 };
