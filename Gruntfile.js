e79517c23bb96b4fa40a3d002a14640605b84a3e
Praseetha-KR <voidimagineer@yahoo.com>
2015-09-27 17:25:37 +0530

    removed unwanted

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,5 +99,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['uglify', 'concurrent:target']);
-    grunt.registerTask('production', ['compass', 'jekyll:prod']);
+    grunt.registerTask('production', ['compass', 'concat', 'cssmin', 'uglify', 'jekyll:prod']);
 };
--- src/README.md
+++
@@ -1,9 +0,0 @@
-# Imagineer [![Build Status](https://travis-ci.org/Praseetha-KR/imagineer.svg?branch=master)](https://travis-ci.org/Praseetha-KR/imagineer)
-
-[Imagineer.in](https://imagineer.in) is build on top of jekyll.
-
-It uses grunt to build.
-
-Requirements :-
- - Ruby 2.0 and above
- - Node (For grunt)
